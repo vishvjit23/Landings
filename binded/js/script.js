3d852988f67217ad428d1e286bf5f83ddab790c6
vishvjit vaghela <95084730+vishvjit23@users.noreply.github.com>
2024-11-06 10:12:45 +0530

    Add files via upload

---
+++ binded/js/script.js
@@ -0,0 +1,4 @@
+document.getElementById('custom_mobileMenu').addEventListener('click', function() {
+    const links = document.getElementById('custom_navbarLinks');
+    links.classList.toggle('active');
+});
